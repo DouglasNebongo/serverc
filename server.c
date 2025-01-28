98bda7108fc80aeb756552456008ee7273cf8635
FermatD <mnebongo@gmail.com>
2025-01-28 16:14:07 -0500

    initial commit: added basic project files and structure

---
+++ server.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 4096
+
+// Structure to store HTTP headers
+typedef struct {
+    char key[256];
+    char value[256];
+} Header;
+
+
+
+int main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
+    // Create socket
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind socket
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind failed");
+        close(server_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for connections
+    if (listen(server_socket, 5) < 0) {
+        perror("listen failed");
+        close(server_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server is running on http://localhost:%d\n", PORT);
+
+    while (1) {
+        // Accept a connection
+        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_len);
+        if (client_socket < 0) {
+            perror("accept failed");
+            continue;
+        }
+
+        // Read the request
+        char request[BUFFER_SIZE];
+        ssize_t bytes_read = recv(client_socket, request, BUFFER_SIZE - 1, 0);
+        if (bytes_read < 0) {
+            perror("recv failed");
+            close(client_socket);
+            continue;
+        }
+        request[bytes_read] = '\0';
+
+        //log request to console
+        printf("%s", request);
+
+
+        // Handle the request
+        handle_request(client_socket, request);
+
+        // Close the client socket
+        close(client_socket);
+    }
+
+    // Close the server socket (this will never be reached in this example)
+    close(server_socket);
+    return 0;
+}
